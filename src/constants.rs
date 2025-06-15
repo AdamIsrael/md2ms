c17ae71dfd58dc713b6b5ea4204c531806f14a1e
Adam Israel <adam@adamisrael.com>
2025-06-15 15:10:17 -0400

    Fix the default format/font
    The default should be `Modern`, using `Times New Roman`. `Classic`
    should be `Courier New`

--- src/constants.rs
+++ src/constants.rs
@@ -4,9 +4,9 @@
 // For whatever reason, we have to double the font size to get the right size in the docx
 pub const FONT_SIZE: usize = 24;
 
-pub const DEFAULT_FONT: &str = "Courier New";
+pub const DEFAULT_FONT: &str = "Times New Roman";
 
 pub const FONTS: &[&str] = &[
-    "Courier New",
     "Times New Roman",
+    "Courier New",
 ];
